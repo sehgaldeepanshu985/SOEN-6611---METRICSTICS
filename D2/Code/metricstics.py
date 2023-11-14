1acf361ec6ef0bbd93d8af427f62f47810e4a99b
DEEPANSHU SEHGAL <41449411+sehgaldeepanshu985@users.noreply.github.com>
2023-11-14 01:06:01 -0500

    Update metricstics.py for Exception Handling

--- D2/Code/metricstics.py
+++ D2/Code/metricstics.py
@@ -1,66 +1,142 @@
 class Metricstics:
+    """
+    The Metricstics class provides functionality to calculate various statistical
+    metrics on a list of numerical data.
+    """
+    
     def __init__(self, data):
+        """
+        Initialize the Metricstics object with data after validation and sorting.
+        """
+        self.data = self._initialize_data(data)
+
+    def _initialize_data(self, data):
+        """
+        Validate and sort the data upon initialization.
+        """
+        self._validate_data(data)
+        return self._sort_data(data)
+
+    def _validate_data(self, data):
+        """
+        Validate the data to ensure it is present and contains only numeric values.
+        Raises a ValueError if the data is empty or contains non-numeric values.
+        """
         if not data:
             raise ValueError("Data not present.")
-        self.data = self.manual_sort(data)
+        for value in data:
+            if not isinstance(value, (int, float)):
+                raise ValueError("Not a numerical value")
 
-    # Implementing a simple sorting algorithm
-    def manual_sort(self, data):
+    def _sort_data(self, data):
+        """
+        Sort the data using insertion sort algorithm.
+        """
         for i in range(1, len(data)):
             key = data[i]
             j = i - 1
-            while j >=0 and key < data[j]:
+            while j >= 0 and key < data[j]:
                 data[j + 1] = data[j]
                 j -= 1
             data[j + 1] = key
         return data
 
-    # Implementing manual minimum function
     def minimum(self):
-        return self.data[0]  # Assuming the data is sorted, the first element is the minimum
+        """
+        Return the minimum value in the data.
+        """
+        return self.data[0]
 
-    # Implementing manual maximum function
     def maximum(self):
-        return self.data[-1]  # Assuming the data is sorted, the last element is the maximum
+        """
+        Return the maximum value in the data.
+        """
+        return self.data[-1]
 
-    # Implementing manual mean function
     def mean(self):
-        total = sum(self.data)
+        """
+        Calculate and return the mean of the data.
+        """
+        total = self._manual_sum(self.data)
         return total / len(self.data)
 
-    # Implementing manual median function
     def median(self):
+        """
+        Calculate and return the median of the data.
+        """
         n = len(self.data)
         mid = n // 2
         if n % 2 == 0:
-            # If even number of data points, median is the average of the two middle numbers
             return (self.data[mid - 1] + self.data[mid]) / 2
         else:
-            # If odd number of data points, median is the middle number
             return self.data[mid]
 
-    # Implementing manual mode function
     def mode(self):
-        frequency = {}
-        max_freq = 0
-        mode_val = set()
-        for num in self.data:
-            frequency[num] = frequency.get(num, 0) + 1
-            if frequency[num] > max_freq:
-                max_freq = frequency[num]
-                mode_val = {num}
-            elif frequency[num] == max_freq:
-                mode_val.add(num)
-        return mode_val if len(mode_val) > 1 else mode_val.pop()
+        """
+        Calculate and return the mode(s) of the data.
+        If there are multiple modes, return a list of modes.
+        """
+        frequency = self._calculate_frequency(self.data)
+        max_freq = max(frequency.values())
+        modes = [number for number, freq in frequency.items() if freq == max_freq]
+        return modes if len(modes) > 1 else modes[0]
 
-    # Implementing manual mean absolute deviation function
     def mean_absolute_deviation(self):
+        """
+        Calculate and return the mean absolute deviation of the data.
+        """
         mean_val = self.mean()
-        total_deviation = sum(abs(x - mean_val) for x in self.data)
+        total_deviation = self._manual_sum(abs(x - mean_val) for x in self.data)
         return total_deviation / len(self.data)
 
-    # Implementing manual standard deviation function
     def standard_deviation(self):
+        """
+        Calculate and return the standard deviation of the data.
+        """
         mean_val = self.mean()
-        variance = sum((x - mean_val) ** 2 for x in self.data) / len(self.data)
-        return variance ** 0.5
+        variance = self._manual_sum((x - mean_val) ** 2 for x in self.data) / len(self.data)
+        return self._sqrt(variance)
+
+    def _manual_sum(self, iterable):
+        """
+        Manually sum up an iterable of numbers and return the total.
+        """
+        total = 0
+        for item in iterable:
+            total += item
+        return total
+
+    def _calculate_frequency(self, data):
+        """
+        Calculate the frequency of each value in the data and return a dictionary.
+        """
+        frequency = {}
+        for num in data:
+            frequency[num] = frequency.get(num, 0) + 1
+        return frequency
+
+    def _sqrt(self, value):
+        """
+        Calculate and return the square root of a value using Newton's method.
+        """
+        x = value
+        while True:
+            root = (x + value / x) / 2
+            if abs(root - x) < 0.000001:
+                return root
+            x = root
+
+    # Dummy methods for future features or complex operations
+    def _dummy_complex_operation(self):
+        """
+        Placeholder for a complex operation to be implemented in the future.
+        Currently does nothing.
+        """
+        pass
+
+    def future_feature_placeholder(self):
+        """
+        Another placeholder for potential future expansion of the class.
+        Currently does nothing.
+        """
+        pass
