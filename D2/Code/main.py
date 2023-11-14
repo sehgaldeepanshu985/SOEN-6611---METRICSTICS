2a9f702938347dd62ebe155ed283f9f4df0576f3
DEEPANSHU SEHGAL <41449411+sehgaldeepanshu985@users.noreply.github.com>
2023-11-14 02:46:37 -0500

    Exception Handling Fix

--- D2/Code/main.py
+++ D2/Code/main.py
@@ -4,8 +4,6 @@ from tkinter.scrolledtext import ScrolledText
 from metricstics import Metricstics
 import random
 
-# Helper functions for user input and interface updates
-
 def ask_for_number_of_values():
     try:
         return simpledialog.askinteger("Input", "How many values would you like to generate?", minvalue=1, maxvalue=10000)
@@ -21,7 +19,16 @@ def retrieve_data():
     data_str = data_display.get('1.0', tk.END).strip()
     if not data_str:
         raise ValueError("No data generated.")
-    return list(map(int, data_str.split(',')))
+    
+    data_list = data_str.split(',')
+    data = []
+    for item in data_list:
+        try:
+            num = int(item.strip())
+            data.append(num)
+        except ValueError:
+            raise ValueError("Not a numerical value")
+    return data
 
 def update_label_with_result(label, result):
     label.config(text=result)
@@ -29,13 +36,23 @@ def update_label_with_result(label, result):
 def reset_label(label):
     label.config(text=f"{label.cget('text').split(':')[0]}: ")
 
-# Main application functions
+def show_error_message(message):
+    messagebox.showerror("Error", message)
+
+def validate_numeric_data(data):
+    for value in data:
+        if not isinstance(value, (int, float)):
+            raise ValueError("Not a numerical value")
 
 def generate_random_values():
     numeric_val = ask_for_number_of_values()
     if numeric_val is not None:
-        val = [random.randint(0, 1000) for _ in range(numeric_val)]
-        update_data_display(val)
+        try:
+            val = [random.randint(0, 1000) for _ in range(numeric_val)]
+            validate_numeric_data(val)
+            update_data_display(val)
+        except ValueError as e:
+            show_error_message(str(e))
 
 def calculate_single_metric(metric_function, label):
     try:
@@ -44,7 +61,7 @@ def calculate_single_metric(metric_function, label):
         result = metric_function(metrics)
         update_label_with_result(label, f"{metric_function.__name__.capitalize()}: {result}")
     except ValueError as e:
-        update_label_with_result(label, f"Error: {e}")
+        show_error_message(str(e))
 
 def calculate_all_metrics():
     try:
@@ -52,24 +69,26 @@ def calculate_all_metrics():
         for metric_function, label in zip(stats_functions, stats_labels):
             calculate_single_metric(metric_function[0], label)
     except ValueError as e:
-        for label in stats_labels:
-            reset_label(label)
-            update_label_with_result(label, f"Error: {e}")
+        show_error_message(str(e))
 
 def func_clear_all():
     data_display.delete('1.0', tk.END)
     for label in stats_labels:
         reset_label(label)
 
-# Initialize the main application window
 root = tk.Tk()
 root.title('METRICSTICS')
 root.geometry('800x600')
 
-# Define styles for buttons, labels, and the data display area
-button_style = {'font': ('Arial', 12), 'bg': '#D3D3D3', 'fg': 'black', 'padx': 10, 'pady': 5}
-label_style = {'font': ('Arial', 12), 'bg': '#f0f0f0', 'fg': 'black'}
-data_display_style = {'font': ('Consolas', 10), 'bg': '#fff', 'fg': 'black'}
+button_style = {
+    'font': ('Arial', 12), 'bg': '#D3D3D3', 'fg': 'black', 'padx': 10, 'pady': 5
+}
+label_style = {
+    'font': ('Arial', 12), 'bg': '#f0f0f0', 'fg': 'black'
+}
+data_display_style = {
+    'font': ('Consolas', 10), 'bg': '#fff', 'fg': 'black'
+}
 
 main_frame = tk.Frame(root, bg='#f0f0f0')
 main_frame.pack(expand=True, fill='both', padx=20, pady=20)
@@ -87,7 +106,7 @@ generate_button = tk.Button(buttons_frame, text='Generate Data', command=generat
 generate_button.pack(side=tk.LEFT, padx=5)
 
 calculate_all_button = tk.Button(buttons_frame, text='Calculate All Metrics', command=calculate_all_metrics, **button_style)
-calculate_all_button.pack(side=tk.LEFT, padx=5)
+calculate_all_button.pack(side=tk.LEFT, padx=(5, 20))  # Added padding for visual separation
 
 clear_all_button = tk.Button(buttons_frame, text='Clear All', command=func_clear_all, **button_style)
 clear_all_button.pack(side=tk.LEFT, padx=5)
@@ -110,7 +129,8 @@ for i, (func, name) in enumerate(stats_functions):
     label = tk.Label(metrics_frame, text=f"{name}: ", **label_style)
     label.grid(row=i, column=0, sticky='w', padx=5, pady=2)
     stats_labels.append(label)
-    button = tk.Button(metrics_frame, text=f"Calculate {name}", command=lambda f=func, l=label: calculate_single_metric(f, l), **button_style)
+    button = tk.Button(metrics_frame, text=f"Calculate {name}", 
+                       command=lambda f=func, l=label: calculate_single_metric(f, l), **button_style)
     button.grid(row=i, column=1, sticky='e', padx=5, pady=2)
 
 footer_frame = tk.Frame(root, bg='#ddd', height=30)
